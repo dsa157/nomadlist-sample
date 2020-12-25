8d41e996f79158e767463c850f7e3d5ca1d99617
David April <dsa157@gmail.com>
2020-12-25 15:03:43 -0500

    added side trip handling

--- docs/index.php
+++ docs/index.php
@@ -5,6 +5,16 @@
 </head>
 <body>
 
+<form action="" method="post"> 
+<table><tr> 
+<td><input placeholder="Arrival" type="date" name="arrival"></td> 
+<td><input placeholder="Departure" type="date" name="departure"></td> 
+<td><input placeholder="City" type="text" name="city"></td> 
+<td><input placeholder="Country" type="text" name="country"></td> 
+
+<td><input name="submit" type="submit" value="Add Trip"/></td></tr> 
+</table>
+
 <?php 
 
 include "./Utils.php";
@@ -15,6 +25,11 @@ if ($db == null) {
 }
 
 $tripList = new TripList($db);
+if ($_POST) {
+  //echo "got a POST";
+  $tripList->add();
+}
+
 $tripList->display();
 
 //-------------------------------------------------
@@ -22,26 +37,63 @@ $tripList->display();
 class TripList {
 
   protected $db;
+  protected $trips;
 
   function __construct($db) {
     $this->db = $db;
   }
 
+  function getTrips() {
+    $this->trips = $this->db->query('select * from trips order by arrival');
+  }
+
   function display() {
-    $results = $this->db->query('select * from trips');
-    while ($row = $results->fetchArray()) {
+    if ($this->trips == null) {
+      $this->getTrips();
+    }
+    while ($row = $this->trips->fetchArray()) {
       $this->printRow($row);
-      //var_dump($row);
     }
   }
 
-  function add($arrival, $departure, $city, $country) {
+  function add() {
+    if ($this->trips == null) {
+      $this->getTrips();
+    }
+    $arrival = Utils::formatDateWithFormat($_POST["arrival"], "Y-m-d");
+    $departure = Utils::formatDateWithFormat($_POST["departure"], "Y-m-d");
+    $city = $_POST["city"];
+    $country = $_POST["country"];
+    $sideTrip = 0;
+    while ($row = $this->trips->fetchArray()) {
+      $id1 = $row["id"];
+      $arrival1 = $row["arrival"];
+      $departure1 = $row["departure"];
+      $city1 = $row["city"];
+      $country1 = $row["country"];
+      if ($arrival >= $arrival1 && $departure <= $departure1) {
+        echo "processing side trip and adjusting existing trips";
+        //delete the old record (editing it requires a littel more code logic)
+        $this->delete($id1);
+        //recreate the old record with new dates sandwiching the side trip
+        $this->insert($arrival1, $arrival, $city1, $country1);
+        $this->insert($departure, $departure1, $city1, $country1);
+      }
+    }
+
+    
+    $this->insert($arrival, $departure, $city, $country);
+  }
+
+  function insert($arrival, $departure, $city, $country) {
+    $this->db->exec("insert into trips (arrival, departure, city, country) values ('$arrival', '$departure', '$city', '$country')" );
   }
 
   function edit($tripID, $arrival, $departure, $city, $country) {
   }
 
   function delete($tripID) {
+    $this->db->exec("delete from trips where id = $tripID" );
   }
 
   function printRow($row) {
--- docs/utils.php
+++ docs/utils.php
@@ -6,7 +6,11 @@ class Utils {
   public static function formatDate($str) {
     $date = new DateTime($str);
     return $date->format('d M Y');
+  }
 
+  public static function formatDateWithFormat($str, $format) {
+    $date = new DateTime("$str");
+    return $date->format($format);
   }
 }
 
